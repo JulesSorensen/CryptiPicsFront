4d254b999372c3fdcbed77231fcf4e43ccfa8d8a
Jules Sorensen <julessorens@gmail.com>
2023-03-09 22:28:27 +0100

    assets

--- src/App.vue
+++ src/App.vue
@@ -6,13 +6,10 @@ import HelloWorld from '@components/HelloWorld.vue';
 <template>
   <div>
     <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      <img src="./assets/img/logo/cryptipics.png" class="logo" alt="Vite logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="CryptiPics" />
 </template>
 
 <style scoped>
